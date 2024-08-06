276f4e1293c752986a775adb689c2fb4a079d299
SivaRamaRaju2310 <152994667+SivaRamaRaju2310@users.noreply.github.com>
2024-08-06 22:19:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-var express =require("express")
-var bodyParser=require("body-parser")
-var mangoose=require("mongoose")
-const { default: mongoose } = require("mongoose")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const app=express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"))
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.use(bodyParser.urlencoded(
-    {
-        extended:true
-    }
-))
+var db = mongoose.connection;
 
-mongoose.connect("mongodb://localhost:27017/MoneyList")
-var db=mongoose.connection
+db.on("error", () => console.log("Error in connecting to DB"));
+db.once("open", () => console.log("Connected to Database"));
 
-db.on("error",()=>console.log("error in conning to DB"))
+app.post("/add", (req, res) => {
+    var category_select = req.body.category_select;
+    var amount_input = req.body.amount_input;
+    var info = req.body.info;
+    var date_input = req.body.date_input;
 
-db.once("open",()=>console.log("connected to database"))
-
-app.post("/add",(req,res)=>{
-    var category_select=req.body.category_select;
-    var amount_input=req.body.amount_input;
-    var info=req.body.info;
-    var date_input=req.body.date_input
-
-    var data={
+    var data = {
         "Category": category_select,
-        "Amount":amount_input,
-        "Info":info,
-        "Date":date_input
-    }
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
+        "Amount": amount_input,
+        "Info": info,
+        "Date": date_input
+    };
+
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
-
         }
-        console.log("record inserted successfully")
+        console.log("Record inserted successfully");
+        return res.redirect("/");
+    });
+});
 
-
-    })
-})
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.set({
-        "Allow-access-Allow-Origin":'*'
+        "Allow-access-Allow-Origin": '*'
+    });
+    return res.redirect("index.html");
+}).listen(5000);
 
-    })
-    return res.redirect("index.html")
-
-
-}).listen(5000)
-
-console.log("listening on the port 5000")
+console.log("Listening on port 5000");
